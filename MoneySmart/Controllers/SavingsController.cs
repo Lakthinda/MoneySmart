8eac8b5aeb68592ed9819bdf28c6c4370e2eb161
Lak <lakthinda@gmail.com>
2018-10-01 22:58:29 +1000

    Create MVC and savings controller

---
+++ MoneySmart/Controllers/SavingsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneySmart.API.Controllers
+{
+    [Route("api/savings")]
+    public class SavingsController : Controller
+    {
+        public SavingsController()
+        {
+
+        }
+
+        [HttpGet()]
+        public IActionResult GetSavings()
+        {
+            var result = 1000;
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSavings(int id)
+        {
+            var result = (id==0)? 500 : id ;
+            return Ok(result);
+        }
+    }
+}
