969ccc5ed77d16382b2384e3c16f9323a2855cf9
Lak <lakthinda@gmail.com>
2018-10-19 18:18:19 +1100

    Created RepositoryInterface

--- MoneySmart/Services/ISavingsRepository.cs
+++ MoneySmart/Services/ISavingsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneySmart.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace MoneySmart.API.Services
 {
     interface ISavingsRepository
     {
+        List<SavingAccountWithoutTransactionsDto> GetSavingAccountsWithoutTransactions();
+        double GetTotalSavings();
+        SavingAccountDto GetSavingAccount(int accountId);
+        List<SavingAccountDto> GetSavingAccounts();
+
+
     }
 }
